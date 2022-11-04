85d341a725592fdd74a4ac1d150f9f0e200ee02d
thenaijagamer <sabdullahialaba50@gmail.com>
2022-11-04 17:22:35 +0100

    Setup icon and finish the header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { ReactComponent as MoonIcon } from "../../icons/moon.svg";
 
 class Header extends React.Component {
   render() {
     return (
       <div className="header">
-        <div className="header__logo">Where in the world?</div>
-        <div className="header__theme">
-          <svg class="header__theme-icon">
-            <use xlinkHref="../../icon/sprite.svg#icon-moon"></use>
-          </svg>
-          <span className="header__theme-text">dark mode</span>
+        <div className="header__container">
+          <div className="header__logo">Where in the world?</div>
+          <div className="header__theme">
+            <MoonIcon className="header__theme-icon" />
+            <span className="header__theme-text">Dark mode</span>
+          </div>
         </div>
       </div>
     );
