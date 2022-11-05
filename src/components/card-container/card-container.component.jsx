76a4496c61572ee71e33e1fdc3635b7e6e2dd85c
thenaijagamer <sabdullahialaba50@gmail.com>
2022-11-05 12:43:56 +0100

    Add the card and card-container component

--- src/components/card-container/card-container.component.jsx
+++ src/components/card-container/card-container.component.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import Card from "../card/card.component";
 
 class CardContainer extends React.Component {
   render() {
-    return <div className="card-container">Card Container COMPONENT</div>;
+    return (
+      <div className="card-container">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    );
   }
 }
 
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,7 +2,23 @@ import React from "react";
 
 class Card extends React.Component {
   render() {
-    return <div className="card">Card COMPONENT</div>;
+    return (
+      <div className="card">
+        <div className="card__img"></div>
+        <div className="card__details">
+          <div className="card__name">Brazil</div>
+          <div className="card__props">
+            <span>Population:</span> 206,135,893
+          </div>
+          <div className="card__props">
+            <span>Region:</span> America
+          </div>
+          <div className="card__props">
+            <span>Capital:</span> Brasilia
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
